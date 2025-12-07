06c4ac82b2c02ee9fed03c25beba55b411b1e393
Sithija Shehara <nsithijashehara@gmail.com>
2025-12-07 07:08:25 +0530

    config related issues fixed

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,14 +1,1 @@
-const getEnv = (
-  key: string,
-  defaultValue?: string | number
-): string | number => {
-  const value = process.env[key] || defaultValue;
-
-  if (value === undefined) {
-    throw Error(`Missing String environment variable for ${key}`);
-  }
-
-  return value;
-};
-
-export const PORT = getEnv("VITE_PORT", 3001);
+export const PORT = import.meta.env.VITE_PORT || 2001;
--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
-import { PORT } from "./src/constants/env";
+import { PORT } from "./src/config/env-loader";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    federation({
-      name: "remote_app",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./Button": "./src/components/Button",
-        "./Header": "./src/components/Header",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  server: {
-    port: Number(PORT), // Your desired port
-    strictPort: true, // Exit if port is already in use (optional)
-    host: true, // Listen on all addresses (optional)
-  },
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-  preview: {
-    port: Number(PORT),
-    strictPort: true,
-    cors: true,
-  },
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: "remote_app",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./Button": "./src/components/Button",
+          "./Header": "./src/components/Header",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    server: {
+      port: PORT, // Your desired port
+      strictPort: true, // Exit if port is already in use (optional)
+      host: true, // Listen on all addresses (optional)
+    },
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+    preview: {
+      port: PORT,
+      strictPort: true,
+      cors: true,
+    },
+  };
 });
